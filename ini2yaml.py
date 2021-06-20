5ffab7755f028753f99cd14e23d50e1c61b2f68a
Davi Petris <davispetris@gmail.com>
2021-06-20 04:39:20 -0300

    linting with black

--- ini2yaml.py
+++ ini2yaml.py
@@ -3,9 +3,8 @@ from ansible.inventory.manager import InventoryManager
 import yaml
 import sys
 
-IGNORABLE_VARS = ['inventory_file', 'inventory_dir']
-output = {'all':{'children': {}}}
-
+IGNORABLE_VARS = ["inventory_file", "inventory_dir"]
+output = {"all": {"children": {}}}
 
 
 def parse_host(host):
@@ -13,26 +12,26 @@ def parse_host(host):
     for var_name, var in host.vars.items():
         if host.name == "localhost":
             if not var_name in IGNORABLE_VARS:
-                vars_dict[var_name] = var    
+                vars_dict[var_name] = var
         elif var != inventory.localhost.vars.get(var_name):
             vars_dict[var_name] = var
     host_dict = {host.name: vars_dict}
     return host_dict
 
+
 def parse_group(group):
     group_dict = {}
     # print(f"\n\n\nGroup: {group.name}")
     if len(group.hosts) > 0:
-        group_dict['hosts'] = {}
+        group_dict["hosts"] = {}
         for host in group.hosts:
             # print("\n\n")
             # print(host.serialize())
             parsed_host = parse_host(host)
-            group_dict['hosts'].update(parsed_host)
+            group_dict["hosts"].update(parsed_host)
     if len(group.vars) > 0:
-        group_dict['vars'] = {}
-        group_dict['vars'].update(group.serialize()['vars'])
-
+        group_dict["vars"] = {}
+        group_dict["vars"].update(group.serialize()["vars"])
 
     return group_dict
 
@@ -48,9 +47,9 @@ loader = DataLoader()
 inventory = InventoryManager(loader=loader, sources=inventory_path)
 
 for group_name, group in inventory.groups.items():
-    if group_name == 'all':
+    if group_name == "all":
         output[group_name].update(parse_group(group))
     else:
-        output['all']['children'][group_name] = parse_group(group)
+        output["all"]["children"][group_name] = parse_group(group)
 
 print(yaml.dump(output, sort_keys=False))
